2487a7060af4352f5158554164d444415c900a8f
Abdul Basit <dev.basit2020@gmail.com>
2025-06-16 19:05:54 +0500

    added trusted section

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,15 @@ import { Services } from "@/components/home/services/services";
 import { AboutUs } from "@/components/home/about-us/about-us";
 import { Clients } from "@/components/home/clients/clients";
 import { ContactUs } from "@/components/home/contact-us/contact-us";
+import { Trusted } from "@/components/home/trusted/trusted";
 
 export default function Home() {
   return (
     <>
       <Featured />
-      {/* <Companies /> */}
       <Services />
+      <Trusted />
+      {/* <Companies /> */}
       {/* <AboutUs /> */}
       {/* <Clients /> */}
       {/* <ContactUs /> */}
