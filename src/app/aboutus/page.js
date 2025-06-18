bc9a8b0f3fd23c39c0fb97828fd2654f7f4fb12c
Abdul Basit <dev.basit2020@gmail.com>
2025-06-18 17:24:14 +0500

    added aboutus values

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,8 +1,9 @@
 import { AboutUsFeatured } from "@/components/aboutus/about-us-featured/about-us-featured";
 import { Mission } from "@/components/aboutus/mission/mission";
+import { AboutUsCards } from "@/components/aboutus/about-us-cards/about-us-cards";
+import { Values } from "@/components/aboutus/values/values";
 import { Clients } from "@/components/home/clients/clients";
 import { FAQS } from "@/components/aboutus/faqs/faqs";
-import AboutUsCards from "@/components/aboutus/about-us-cards/about-us-cards";
 
 export default function About_Us() {
   return (
@@ -10,6 +11,7 @@ export default function About_Us() {
       <AboutUsFeatured />
       <Mission />
       <AboutUsCards />
+      <Values />
       {/* <Clients /> */}
       {/* <FAQS /> */}
     </>
