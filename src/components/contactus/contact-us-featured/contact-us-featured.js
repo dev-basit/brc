a6d93c75e43003b042613a7f75140ba55a02dff4
Abdul Basit <dev.basit2020@gmail.com>
2025-06-18 15:13:44 +0500

    bugfixes

--- src/components/contactus/contact-us-featured/contact-us-featured.js
+++ src/components/contactus/contact-us-featured/contact-us-featured.js
@@ -5,7 +5,7 @@ export const ContactUsFeatured = () => {
     <div className={styles.contactUsContainer} data-aos="fade-in">
       <div className={styles.headings}>
         <h1>Get in Touch</h1>
-        <p>Want to get in touch? We'd love to hear from you. Here's how you can reach us</p>
+        <p>Want to get in touch? We&apos;d love to hear from you. Here&apos;s how you can reach us</p>
       </div>
     </div>
   );
