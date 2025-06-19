6e6bca23aad3a7ca56faa9dad3f6a193433a6b14
Abdul Basit <dev.basit2020@gmail.com>
2025-06-19 20:25:53 +0500

    bugfixed

--- src/components/contactus/contact-us-featured/contact-us-featured.js
+++ src/components/contactus/contact-us-featured/contact-us-featured.js
@@ -1,9 +1,13 @@
 import styles from "./contact-us-featured.module.css";
+import commonStyles from "@/app/common.module.css";
 
 export const ContactUsFeatured = () => {
   return (
     <div className={styles.contactUsContainer} data-aos="fade-in">
       <div className={styles.headings}>
+        <p className={commonStyles.textDashedWhite} style={{ textAlign: "center" }}>
+          Contact us
+        </p>
         <h1>Get in Touch</h1>
         <p>Want to get in touch? We&apos;d love to hear from you. Here&apos;s how you can reach us</p>
       </div>
